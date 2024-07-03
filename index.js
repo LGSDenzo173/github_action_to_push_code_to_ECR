09ac698899ca4430ab17720d3326979aff93e1bb
LGSDenzo173 <johnyb574@gmai.com>
2024-07-03 12:08:39 -0700

    comments added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 // middleware 
 const authAdmin = require("./middleware/auth");
-
+//this is just a dummy code
 // const auth_data = require('./middleware/basic-auth');
 const sankeyData = require('./routes/getallsankeys')
 const GetAllsankeyValues = require('./routes/getsankeyvalues')
